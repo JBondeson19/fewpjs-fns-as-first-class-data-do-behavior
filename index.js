622241899f4898ff209a37fd7137d2c763e770cd
JBondeson19 <jpbond58@gmail.com>
2020-11-18 16:24:58 -0500

    'clock'

--- index.js
+++ index.js
@@ -9,5 +9,21 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
+function greet(timeString){
+  
+  const hour = parseInt(timeString, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+  // takes string argument, which specifies 24 hr time
+  //if time is earler than 12, return "good morning"
+  //if time is b/w 12-5pm, return 'good afternoon'
+  // if time is later than 5pm, return 'good evening'
+}
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
